b2d71b25826cacc0a68f3812b66ec5530d531e4a
Félix Laviéville <lavieville.felix@gmail.com>
2023-08-14 12:35:15 +0200

    open cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: '*', // Domaine autorisé à accéder aux ressources
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Autorise les cookies et les en-têtes d'authentification
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
